a2f8162b68d14cff71635ce6c95f486f2f55a3bb
rishitha999 <rishitha.uppunuri@gmail.com>
2017-09-10 14:33:32 +0530

    Update Index.js

--- simpleapp/Index.js
+++ simpleapp/Index.js
@@ -1,8 +1,28 @@
-var express =require("express");
-var app= express();
-var port = process.env.PORT || 3000;
+const express =require('express');
+const bodyParser=require('body-parser');
+const request = require('request');
 
-app.get("/", function(req,res){
-    res.send("welcome to  node js app on Heroku ");
-});
-app.listen(port);
+const app=express()
+
+app.set('port',(process.env.PORT || 5000))
+
+//allows us to process the data
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//routes
+app.get('/',function(req,res){
+    res.send("Hi I am a CHATBOT")
+})
+//facebook
+
+app.get('/webhook/',function(req,res) {
+    if(req.query['hub.verify_token'] === "blondiebytes"){
+        res.send(req.query['hub.challange'])
+    }
+    res.send("Wrong token");
+
+    })
+app.listen(app.get('port'),function(){
+    console.log("its running");
+})
